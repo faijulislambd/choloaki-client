b1621f05680ea6760567f727c21241ea05788ec9
faijul144 <f.faijulislam144@gmail.com>
2023-06-09 20:48:37 +0600

    Instructors page ready

--- src/pages/Instructors/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors/Instructors.jsx
@@ -1,10 +1,21 @@
+import HeadTitle from "../../../components/HeadTitle";
 import PageTitle from "../../../components/PageTitle";
+import useInstructors from "../../../hooks/useInstructors";
+import UserCard from "../../Shared/UserCard/UserCard";
 
 const Instructors = () => {
+  const [instructors] = useInstructors();
   return (
     <>
       <PageTitle title="Instructors"></PageTitle>
-      <div>Teachers</div>
+      <section className="py-10">
+        <HeadTitle first="Our Esteemed" last="Instructors"></HeadTitle>
+        <div className="grid gap-4 gap-y-10 grid-cols md:grid-cols-3">
+          {instructors.map((instructor) => (
+            <UserCard user={instructor} key={instructor._id}></UserCard>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
